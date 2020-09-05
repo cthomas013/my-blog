950232ac23cbb24f57556db233f4df5efb191321
Cory Thomas <cmthomas@email.wm.edu>
2020-09-05 14:35:45 -0400

    initial my-blog commit

---
+++ src/build/precache-manifest.af53a3dd96cc8d0a18f2f6cc4d865b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abdf2e57d6cde4efd8da3eeeb35b818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f931c3060d4ad84288fc",
+    "url": "/static/css/main.0d1144d2.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f931c3060d4ad84288fc",
+    "url": "/static/js/main.f22cb6aa.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
